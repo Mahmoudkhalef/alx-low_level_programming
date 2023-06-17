a5ce41ea493846850e76c4876d0bb6a1390661c3
mahmoudkhalef <mahmoudkhalefmmmmmmmmmmmm9@gmail.com>
2023-06-17 03:12:32 -0700

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,8 +5,7 @@
 /**
  * main - entry point
  *
- * description: print the value of n status:
- *		greater than, is zero and is not less than 6.
+ * description: print the last ddigit of the numper stored in the variable.
  *
  * return: 0 (success)
 */
@@ -18,12 +17,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	digit = n % 10;
-
 	if (digit > 5)
 		printf("last digit of %i is %i and is greater than 5\n", n, digit);
 	else if (digit == 0)
 		printf("last digit of %i is %i and is 0\n", n, digit);
-	else if (digit < 6 && digit != 0)
-		printf("last digit of %i is %i and is less than 6 and not 0\n", n, digit);
+	else if (digit < 6 && digit != 0
+		printf("last digit of %i is %i amd isnless than 6 and not 0\n", n, digit);
 	return (0);
-}
