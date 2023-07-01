5f2336c3a7d8b1cefeca703ec6da2a32933a907e
mahmoudkhalef <mahmoudkhalefmmmmmmmmmmmm9@gmail.com>
2023-06-30 20:01:55 -0700

    m

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -36,7 +36,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (j >= 0)
 			s = 0;
 		r[k] = (f + s + d) % 10 + '0'
-			d = (f + s + d) 10;
+		d = (f + s + d) 10;
 	}
 	if (d == 1)
 	{
