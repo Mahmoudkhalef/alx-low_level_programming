942e75c4978a032a2d1ddc114724014f2d7a5e35
mahmoudkhalef <mahmoudkhalefmmmmmmmmmmmm9@gmail.com>
2023-07-03 18:10:07 -0700

    m

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,7 +4,7 @@
  * _strpbrk - Search a string for any of a set of bytes.
  * @s: string
  * @accept: string to match
- * Return: pointer to the byte in s that matches one of the bytes in accept
+ * Return: Pointer to the byte in s that matches one of the bytes in accept
  * or NULL if no such byte is found.
  */
 
@@ -30,5 +30,4 @@ char *_strpbrk(char *s, char *accept)
 	}
 
 	return (0);
-
 }
