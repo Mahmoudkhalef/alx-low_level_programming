9f520a22a1e292f0c9c69404b1177801a2207134
mahmoudkhalef <mahmoudkhalefmmmmmmmmmmmm9@gmail.com>
2023-07-03 20:00:42 -0700

    m

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,33 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strpbrk - Search a string for any of a set of bytes.
- * @s: string
- * @accept: string to match
- * Return: Pointer to the byte in s that matches one of the bytes in accept
- * or NULL if no such byte is found.
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
-char *_strpbrk(char *s, char *accept)
+int main(void)
 {
-	int itr, jtr;
-	char *p;
+	char *s = "hello, world";
+	char *f = "world";
+	char*t;
 
-	itr = 0;
-	while (s[itr] != '\0')
-	{
-		jtr = 0;
-		while (accept[jtr] != '\0')
-		{
-			if (accept[jtr] == s[itr])
-			{
-				p = &s[itr];
-				return (p);
-			}
-			jtr++;
-		}
-		itr++;
-	}
-
+	t = _strstr(s, f);
+	printf("%s\n", t);
 	return (0);
 }
