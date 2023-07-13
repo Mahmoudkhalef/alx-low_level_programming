4c72edaad1e141425f3c9d78619d829e9c303fe7
mahmoudkhalef <mahmoudkhalefmmmmmmmmmmmm9@gmail.com>
2023-07-13 06:31:37 -0700

    malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -30,7 +30,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
 
-	if (size == 0 || nmemb == 0)
+	if (size == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(sizeof(int) * nmemb);
 
