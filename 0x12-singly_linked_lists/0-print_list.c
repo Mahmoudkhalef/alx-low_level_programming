c63698143f734c725d22a97c6d22a439b64516b2
mahmoud khalef <mahmoudkhalefm9@gmail.com>
2023-07-28 01:17:18 -0700

    linked

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,14 +2,13 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: the string whose length too check
+ * @s: the string whose length to check
  *
  * Return: integer length of string
  */
 int _strlen(char *s)
 {
 	int i = 0;
-
 
 	if (!s)
 		return (0);
