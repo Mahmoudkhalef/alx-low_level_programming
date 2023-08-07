532cd7bb3c54f9bdc82357009218be75afd11e12
mahmoud khalef <mahmoudkhalefm9@gmail.com>
2023-08-07 09:31:33 -0700

    m

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@ int _strlen(char *s)
 {
 	int i = 0;
 
-	if (is)
+	if (!s)
 		return (0);
 
 	while (*s++)
@@ -24,17 +24,17 @@ int _strlen(char *s)
  * @text_content: text to write
  *
  * Return: 1 on success 0 on failure
-*/
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
-	return (-1);
+		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IWUSR);
 	if (fd == -1)
-	return (-1);
+		return (-1);
 	if (len)
 	bytes = write(fd, text_content, len);
 	close(fd);
