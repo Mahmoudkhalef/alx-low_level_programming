278996b6fc1173db0a0118ebb80fde5bd0d363d9
mahmoud khalef <mahmoudkhalefm9@gmail.com>
2023-08-07 18:20:22 -0700

    m

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * read_textfile - reads text from a file and prints it
+ * read_textfile - reaads text from a afile and prints it
  * @filename: name of file to read
  * @letters: number of bytes to read
  *
@@ -15,7 +15,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (!filename || !letters)
 		return (0);
-	fd - open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	bytes = read(fd, &buf[0], letters);
@@ -23,3 +23,4 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	close(fd);
 	return (bytes);
 }
+	
